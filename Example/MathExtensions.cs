184d5c50fb933a70a00cb1487147b2bc2630336c
ndrwrbgs <ndrwrbgs@users.noreply.github.com>
2018-12-09 15:47:38 -0800

    Create MathExtensions.cs

---
+++ Example/MathExtensions.cs
@@ -0,0 +1,415 @@
+namespace SystemExtensions
+{
+	public static class MathExtensions
+	{
+
+
+		/// <inheritdoc cref="System.Math.Acos(System.Double)" />
+		public static System.Double Acos(this System.Double d)
+		{
+			return System.Math.Acos(d);
+		}
+
+		/// <inheritdoc cref="System.Math.Asin(System.Double)" />
+		public static System.Double Asin(this System.Double d)
+		{
+			return System.Math.Asin(d);
+		}
+
+		/// <inheritdoc cref="System.Math.Atan(System.Double)" />
+		public static System.Double Atan(this System.Double d)
+		{
+			return System.Math.Atan(d);
+		}
+
+		/// <inheritdoc cref="System.Math.Atan2(System.Double, System.Double)" />
+		public static System.Double Atan2(this System.Double y, System.Double x)
+		{
+			return System.Math.Atan2(y, x);
+		}
+
+		/// <inheritdoc cref="System.Math.Ceiling(System.Decimal)" />
+		public static System.Decimal Ceiling(this System.Decimal d)
+		{
+			return System.Math.Ceiling(d);
+		}
+
+		/// <inheritdoc cref="System.Math.Ceiling(System.Double)" />
+		public static System.Double Ceiling(this System.Double a)
+		{
+			return System.Math.Ceiling(a);
+		}
+
+		/// <inheritdoc cref="System.Math.Cos(System.Double)" />
+		public static System.Double Cos(this System.Double d)
+		{
+			return System.Math.Cos(d);
+		}
+
+		/// <inheritdoc cref="System.Math.Cosh(System.Double)" />
+		public static System.Double Cosh(this System.Double value)
+		{
+			return System.Math.Cosh(value);
+		}
+
+		/// <inheritdoc cref="System.Math.Floor(System.Decimal)" />
+		public static System.Decimal Floor(this System.Decimal d)
+		{
+			return System.Math.Floor(d);
+		}
+
+		/// <inheritdoc cref="System.Math.Floor(System.Double)" />
+		public static System.Double Floor(this System.Double d)
+		{
+			return System.Math.Floor(d);
+		}
+
+		/// <inheritdoc cref="System.Math.Sin(System.Double)" />
+		public static System.Double Sin(this System.Double a)
+		{
+			return System.Math.Sin(a);
+		}
+
+		/// <inheritdoc cref="System.Math.Tan(System.Double)" />
+		public static System.Double Tan(this System.Double a)
+		{
+			return System.Math.Tan(a);
+		}
+
+		/// <inheritdoc cref="System.Math.Sinh(System.Double)" />
+		public static System.Double Sinh(this System.Double value)
+		{
+			return System.Math.Sinh(value);
+		}
+
+		/// <inheritdoc cref="System.Math.Tanh(System.Double)" />
+		public static System.Double Tanh(this System.Double value)
+		{
+			return System.Math.Tanh(value);
+		}
+
+		/// <inheritdoc cref="System.Math.Round(System.Double)" />
+		public static System.Double Round(this System.Double a)
+		{
+			return System.Math.Round(a);
+		}
+
+		/// <inheritdoc cref="System.Math.Round(System.Double, System.Int32)" />
+		public static System.Double Round(this System.Double value, System.Int32 digits)
+		{
+			return System.Math.Round(value, digits);
+		}
+
+		/// <inheritdoc cref="System.Math.Round(System.Double, System.MidpointRounding)" />
+		public static System.Double Round(this System.Double value, System.MidpointRounding mode)
+		{
+			return System.Math.Round(value, mode);
+		}
+
+		/// <inheritdoc cref="System.Math.Round(System.Double, System.Int32, System.MidpointRounding)" />
+		public static System.Double Round(this System.Double value, System.Int32 digits, System.MidpointRounding mode)
+		{
+			return System.Math.Round(value, digits, mode);
+		}
+
+		/// <inheritdoc cref="System.Math.Round(System.Decimal)" />
+		public static System.Decimal Round(this System.Decimal d)
+		{
+			return System.Math.Round(d);
+		}
+
+		/// <inheritdoc cref="System.Math.Round(System.Decimal, System.Int32)" />
+		public static System.Decimal Round(this System.Decimal d, System.Int32 decimals)
+		{
+			return System.Math.Round(d, decimals);
+		}
+
+		/// <inheritdoc cref="System.Math.Round(System.Decimal, System.MidpointRounding)" />
+		public static System.Decimal Round(this System.Decimal d, System.MidpointRounding mode)
+		{
+			return System.Math.Round(d, mode);
+		}
+
+		/// <inheritdoc cref="System.Math.Round(System.Decimal, System.Int32, System.MidpointRounding)" />
+		public static System.Decimal Round(this System.Decimal d, System.Int32 decimals, System.MidpointRounding mode)
+		{
+			return System.Math.Round(d, decimals, mode);
+		}
+
+		/// <inheritdoc cref="System.Math.Truncate(System.Decimal)" />
+		public static System.Decimal Truncate(this System.Decimal d)
+		{
+			return System.Math.Truncate(d);
+		}
+
+		/// <inheritdoc cref="System.Math.Truncate(System.Double)" />
+		public static System.Double Truncate(this System.Double d)
+		{
+			return System.Math.Truncate(d);
+		}
+
+		/// <inheritdoc cref="System.Math.Sqrt(System.Double)" />
+		public static System.Double Sqrt(this System.Double d)
+		{
+			return System.Math.Sqrt(d);
+		}
+
+		/// <inheritdoc cref="System.Math.Log(System.Double)" />
+		public static System.Double Log(this System.Double d)
+		{
+			return System.Math.Log(d);
+		}
+
+		/// <inheritdoc cref="System.Math.Log10(System.Double)" />
+		public static System.Double Log10(this System.Double d)
+		{
+			return System.Math.Log10(d);
+		}
+
+		/// <inheritdoc cref="System.Math.Exp(System.Double)" />
+		public static System.Double Exp(this System.Double d)
+		{
+			return System.Math.Exp(d);
+		}
+
+		/// <inheritdoc cref="System.Math.Pow(System.Double, System.Double)" />
+		public static System.Double Pow(this System.Double x, System.Double y)
+		{
+			return System.Math.Pow(x, y);
+		}
+
+		/// <inheritdoc cref="System.Math.IEEERemainder(System.Double, System.Double)" />
+		public static System.Double IEEERemainder(this System.Double x, System.Double y)
+		{
+			return System.Math.IEEERemainder(x, y);
+		}
+
+		/// <inheritdoc cref="System.Math.Abs(System.SByte)" />
+		public static System.SByte Abs(this System.SByte value)
+		{
+			return System.Math.Abs(value);
+		}
+
+		/// <inheritdoc cref="System.Math.Abs(System.Int16)" />
+		public static System.Int16 Abs(this System.Int16 value)
+		{
+			return System.Math.Abs(value);
+		}
+
+		/// <inheritdoc cref="System.Math.Abs(System.Int32)" />
+		public static System.Int32 Abs(this System.Int32 value)
+		{
+			return System.Math.Abs(value);
+		}
+
+		/// <inheritdoc cref="System.Math.Abs(System.Int64)" />
+		public static System.Int64 Abs(this System.Int64 value)
+		{
+			return System.Math.Abs(value);
+		}
+
+		/// <inheritdoc cref="System.Math.Abs(System.Single)" />
+		public static System.Single Abs(this System.Single value)
+		{
+			return System.Math.Abs(value);
+		}
+
+		/// <inheritdoc cref="System.Math.Abs(System.Double)" />
+		public static System.Double Abs(this System.Double value)
+		{
+			return System.Math.Abs(value);
+		}
+
+		/// <inheritdoc cref="System.Math.Abs(System.Decimal)" />
+		public static System.Decimal Abs(this System.Decimal value)
+		{
+			return System.Math.Abs(value);
+		}
+
+		/// <inheritdoc cref="System.Math.Max(System.SByte, System.SByte)" />
+		public static System.SByte Max(this System.SByte val1, System.SByte val2)
+		{
+			return System.Math.Max(val1, val2);
+		}
+
+		/// <inheritdoc cref="System.Math.Max(System.Byte, System.Byte)" />
+		public static System.Byte Max(this System.Byte val1, System.Byte val2)
+		{
+			return System.Math.Max(val1, val2);
+		}
+
+		/// <inheritdoc cref="System.Math.Max(System.Int16, System.Int16)" />
+		public static System.Int16 Max(this System.Int16 val1, System.Int16 val2)
+		{
+			return System.Math.Max(val1, val2);
+		}
+
+		/// <inheritdoc cref="System.Math.Max(System.UInt16, System.UInt16)" />
+		public static System.UInt16 Max(this System.UInt16 val1, System.UInt16 val2)
+		{
+			return System.Math.Max(val1, val2);
+		}
+
+		/// <inheritdoc cref="System.Math.Max(System.Int32, System.Int32)" />
+		public static System.Int32 Max(this System.Int32 val1, System.Int32 val2)
+		{
+			return System.Math.Max(val1, val2);
+		}
+
+		/// <inheritdoc cref="System.Math.Max(System.UInt32, System.UInt32)" />
+		public static System.UInt32 Max(this System.UInt32 val1, System.UInt32 val2)
+		{
+			return System.Math.Max(val1, val2);
+		}
+
+		/// <inheritdoc cref="System.Math.Max(System.Int64, System.Int64)" />
+		public static System.Int64 Max(this System.Int64 val1, System.Int64 val2)
+		{
+			return System.Math.Max(val1, val2);
+		}
+
+		/// <inheritdoc cref="System.Math.Max(System.UInt64, System.UInt64)" />
+		public static System.UInt64 Max(this System.UInt64 val1, System.UInt64 val2)
+		{
+			return System.Math.Max(val1, val2);
+		}
+
+		/// <inheritdoc cref="System.Math.Max(System.Single, System.Single)" />
+		public static System.Single Max(this System.Single val1, System.Single val2)
+		{
+			return System.Math.Max(val1, val2);
+		}
+
+		/// <inheritdoc cref="System.Math.Max(System.Double, System.Double)" />
+		public static System.Double Max(this System.Double val1, System.Double val2)
+		{
+			return System.Math.Max(val1, val2);
+		}
+
+		/// <inheritdoc cref="System.Math.Max(System.Decimal, System.Decimal)" />
+		public static System.Decimal Max(this System.Decimal val1, System.Decimal val2)
+		{
+			return System.Math.Max(val1, val2);
+		}
+
+		/// <inheritdoc cref="System.Math.Min(System.SByte, System.SByte)" />
+		public static System.SByte Min(this System.SByte val1, System.SByte val2)
+		{
+			return System.Math.Min(val1, val2);
+		}
+
+		/// <inheritdoc cref="System.Math.Min(System.Byte, System.Byte)" />
+		public static System.Byte Min(this System.Byte val1, System.Byte val2)
+		{
+			return System.Math.Min(val1, val2);
+		}
+
+		/// <inheritdoc cref="System.Math.Min(System.Int16, System.Int16)" />
+		public static System.Int16 Min(this System.Int16 val1, System.Int16 val2)
+		{
+			return System.Math.Min(val1, val2);
+		}
+
+		/// <inheritdoc cref="System.Math.Min(System.UInt16, System.UInt16)" />
+		public static System.UInt16 Min(this System.UInt16 val1, System.UInt16 val2)
+		{
+			return System.Math.Min(val1, val2);
+		}
+
+		/// <inheritdoc cref="System.Math.Min(System.Int32, System.Int32)" />
+		public static System.Int32 Min(this System.Int32 val1, System.Int32 val2)
+		{
+			return System.Math.Min(val1, val2);
+		}
+
+		/// <inheritdoc cref="System.Math.Min(System.UInt32, System.UInt32)" />
+		public static System.UInt32 Min(this System.UInt32 val1, System.UInt32 val2)
+		{
+			return System.Math.Min(val1, val2);
+		}
+
+		/// <inheritdoc cref="System.Math.Min(System.Int64, System.Int64)" />
+		public static System.Int64 Min(this System.Int64 val1, System.Int64 val2)
+		{
+			return System.Math.Min(val1, val2);
+		}
+
+		/// <inheritdoc cref="System.Math.Min(System.UInt64, System.UInt64)" />
+		public static System.UInt64 Min(this System.UInt64 val1, System.UInt64 val2)
+		{
+			return System.Math.Min(val1, val2);
+		}
+
+		/// <inheritdoc cref="System.Math.Min(System.Single, System.Single)" />
+		public static System.Single Min(this System.Single val1, System.Single val2)
+		{
+			return System.Math.Min(val1, val2);
+		}
+
+		/// <inheritdoc cref="System.Math.Min(System.Double, System.Double)" />
+		public static System.Double Min(this System.Double val1, System.Double val2)
+		{
+			return System.Math.Min(val1, val2);
+		}
+
+		/// <inheritdoc cref="System.Math.Min(System.Decimal, System.Decimal)" />
+		public static System.Decimal Min(this System.Decimal val1, System.Decimal val2)
+		{
+			return System.Math.Min(val1, val2);
+		}
+
+		/// <inheritdoc cref="System.Math.Log(System.Double, System.Double)" />
+		public static System.Double Log(this System.Double a, System.Double newBase)
+		{
+			return System.Math.Log(a, newBase);
+		}
+
+		/// <inheritdoc cref="System.Math.Sign(System.SByte)" />
+		public static System.Int32 Sign(this System.SByte value)
+		{
+			return System.Math.Sign(value);
+		}
+
+		/// <inheritdoc cref="System.Math.Sign(System.Int16)" />
+		public static System.Int32 Sign(this System.Int16 value)
+		{
+			return System.Math.Sign(value);
+		}
+
+		/// <inheritdoc cref="System.Math.Sign(System.Int32)" />
+		public static System.Int32 Sign(this System.Int32 value)
+		{
+			return System.Math.Sign(value);
+		}
+
+		/// <inheritdoc cref="System.Math.Sign(System.Int64)" />
+		public static System.Int32 Sign(this System.Int64 value)
+		{
+			return System.Math.Sign(value);
+		}
+
+		/// <inheritdoc cref="System.Math.Sign(System.Single)" />
+		public static System.Int32 Sign(this System.Single value)
+		{
+			return System.Math.Sign(value);
+		}
+
+		/// <inheritdoc cref="System.Math.Sign(System.Double)" />
+		public static System.Int32 Sign(this System.Double value)
+		{
+			return System.Math.Sign(value);
+		}
+
+		/// <inheritdoc cref="System.Math.Sign(System.Decimal)" />
+		public static System.Int32 Sign(this System.Decimal value)
+		{
+			return System.Math.Sign(value);
+		}
+
+		/// <inheritdoc cref="System.Math.BigMul(System.Int32, System.Int32)" />
+		public static System.Int64 BigMul(this System.Int32 a, System.Int32 b)
+		{
+			return System.Math.BigMul(a, b);
+		} 
+	}
+}
